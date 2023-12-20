913e6322ad955168dddf73b57cc321bef2843bf2
tasharenwick <tcrenwick50@gmail.com>
2023-12-20 09:57:50 +0000

    Working on print

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,3 +1,17 @@
-ls = list(range(0,81))
+import random
 
-print(ls)
+import numpy as np
+
+rand_list = [random.randint(1,9) for _ in range(0,81)]
+array = np.array(rand_list)
+array = np.reshape(array, (9, 9))
+
+print(array)
+
+print('-'*40)
+
+print('|', end=' ')
+for num in array[0,:]:
+    print(num, '|', end=' ')
+
+print('-'*40)
